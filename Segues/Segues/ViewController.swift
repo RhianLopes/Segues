685946b59323fbbc8767d9edcc0c7c5c8b83e468
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-07-29 19:54:50 -0300

    feat: introduce initial project

---
+++ Segues/Segues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Rhian Lopes da Costa on 29/07/20.
+//  Copyright © 2020 Rhian Lopes da Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
